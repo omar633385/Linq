a639da607c94bd7601a50df98a09c50e9c233690
3omar63 <om728526@gmail.com>
2025-02-17 12:33:07 +0200

    Anonymous Type

--- Program.cs
+++ Program.cs
@@ -36,13 +36,47 @@
             #region ExtensionMethods
             //123
             //321
-            int x = 123;
-            Console.WriteLine($"Before Reversing => x = {x}");
-             int y=IntExtenstions.reverse(x);
-             y=x.reverse(); //int doesn't have function to reverse [Implementation of class int is readonly]
-                         //solution is Extension Method =>  makes int  gain more capabilites
+            //int x = 123;
+            //Console.WriteLine($"Before Reversing => x = {x}");
+            // int y=IntExtenstions.reverse(x);
+            // y=x.reverse(); //int doesn't have function to reverse [Implementation of class int is readonly]
+            //             //solution is Extension Method =>  makes int  gain more capabilites
 
-            Console.WriteLine($"After Reversing => x = {y}");
+            //Console.WriteLine($"After Reversing => x = {y}");
+
+            #endregion
+
+            #region Anonymous Type
+            //create object from class but used only once or twice => No need to create class
+            Employee employee =new Employee() { Id=10,Name="Ahmed",Salary=5000};
+
+            var employee2 = new { Id = 10, Name = "Ahmed", Salary = 5000 }; // Anonymous Type
+
+            //we can use dynamic but may throw exception if you want to access property not in the type
+
+            Console.WriteLine(employee2.GetType().Name); // <>f__AnonymousType0`3
+            //Compiler will create class with these properties  its name will be => AnonymousType0`3
+            // AnonymousType0 means that first AnonymousType compiler has created
+            // `3 means that has 3 properties
+
+            //object from AnonymousType is immutable => can't be changed
+            
+            //employee2.Id = 20; // invalid
+
+            employee2 = new { Id = 10, Name = "Ahmed", Salary = 5000 };
+            // if we want to change object data
+            employee2 = new { Id = 20, employee2.Name, employee2.Salary };
+
+            employee2 = employee2 with { Id = 20 };// c# 10 feature [syntax sugar]
+            Console.WriteLine(employee2.GetType().Name);
+            // The AnonymousType still the same as long as
+            //1. Same Property name [Case Sensetive]
+            //2. same Property order
+
+           var employee02 = new { Id = 50, Name = "hamada", Salary = 8000 };
+           var employee03 = new { Id = 50, name = "hamada", Salary = 8000 };
+           var employee04 = new { Id = 50, Salary = 8000, Name = "hamada" };
+
 
             #endregion
         }
