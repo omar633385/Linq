a9da1a6693724d83ab8d3eca566d1dcbd02a8051
3omar63 <om728526@gmail.com>
2025-02-17 11:01:42 +0200

    var vs dynamic

--- Program.cs
+++ Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Var vs dynamic
+            //string name = "ahmed";
+            #region Var
+
+            //var name = "ahmed";
+            ////compiler can detect variable data type based on its inital value during Compliation Time
+            //var name02; //Invalid => should be inilized but can't be initlized with null
+
+            ////c# strongly type  
+            
+            /// var is used only as local variable not used in class or struct as property or field
+            /// function can't return var or take parameter of type var
+
+            #endregion
+            #region dynamic
+            dynamic name; //valid =>can also be initliazed 
+            //can be initliazed with null
+            name = "hamada";
+            name = 50;
+            //like var in JS
+            //CLR detects datatype based on its last assigned value at RunTime 
+
+            #endregion
+
+            ///Most Recommended is var => compiler can detect variable data type at Compliation Time
+            ///unlike dynamic => CLR detects datatype at  RunTime (unsafe) => May throw exception
+
+            #endregion
         }
     }
 }
