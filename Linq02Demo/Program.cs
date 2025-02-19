e1ebcb3ad40b209af14c9b457f5b7757c1e29f31
3omar63 <om728526@gmail.com>
2025-02-19 17:11:59 +0200

    FilterationOperators  Transformation

---
+++ Linq02Demo/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Security.Cryptography;
+using static Linq02Demo.ListGenerator; // wrote static to use all things in ListGenerator Class instead of writing class name every time to use anything from it
+namespace Linq02Demo
+{
+
+    internal class Program
+    {
+        static void PrintCollection(IEnumerable collection)
+        {
+            foreach (var item in collection)
+            {
+
+                Console.WriteLine(item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            #region DataSetup
+
+            //Console.WriteLine(ProductsList[0]);
+            //Console.WriteLine(CustomersList[0]);
+            #endregion
+
+            #region Filteration-Operators
+
+            #region Get Elements Out Of Stock
+            //fluent syntax
+            //var result= ProductsList.Where(p=>p.UnitsInStock==0);
+
+            // //query syntax
+            // result= from p in ProductsList
+            //         where p.UnitsInStock==0
+            //         select p;
+
+            //PrintCollection(result);
+
+            #endregion
+
+            #region Get Elements In Stock And In Category Of Meat/Poultry
+            //fluent syntax
+            //var result=ProductsList.Where(p => p.UnitsInStock > 0 && p.Category == "Meat/Poultry");
+
+            ////query syntax
+            //   result= from p in ProductsList
+            //    where p.Category== "Meat/Poultry" && p.UnitsInStock > 0 
+            //    select p;
+            //PrintCollection(result);
+
+            #endregion
+
+            #region Get Elements Out Of Stock In First 10 Elements
+            //indexed where valid only in fluent syntax 
+            // used to have an access to to index of elements of sequence
+
+            //var result =ProductsList.Where((p,i)=> p.UnitsInStock==0 && i<10 );
+            //PrintCollection(result);
+            #endregion
+
+
+            #endregion
+
+            #region Transformation-Operators
+            #region Select Product Name
+            //fluent syntax
+           var result= ProductsList.Select(p => p.ProductName);
+            result = from p in ProductsList
+                     select p.ProductName;
+            PrintCollection(result);
+            #endregion
+            #endregion
+        }
+
+    }
+}
