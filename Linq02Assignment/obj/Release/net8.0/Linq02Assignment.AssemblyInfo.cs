e25c110b22324914b48bf9fa68c32ef270a26de8
3omar63 <om728526@gmail.com>
2025-02-25 10:26:31 +0200

    start assignment 03

--- Linq02Assignment/obj/Release/net8.0/Linq02Assignment.AssemblyInfo.cs
+++ Linq02Assignment/obj/Release/net8.0/Linq02Assignment.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Linq02Assignment")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+279025e48928b99b5577fc744f4f7125a98ece50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Linq02Assignment")]
